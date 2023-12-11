a17229f25b55d60c18c7e831ceb82bb28af66f4c
Tony Tregidgo <a.tregidgo@uk.cdw.com>
2023-12-11 13:34:43 +0000

    y

--- provider.tf
+++ provider.tf
@@ -21,9 +21,7 @@ terraform {
 provider "azurerm" {
   features {}
 }
-# terraform {
-#   backend "azurerm" {}
-# }
+
 data "azurerm_client_config" "current" {}
 
 ######################################
